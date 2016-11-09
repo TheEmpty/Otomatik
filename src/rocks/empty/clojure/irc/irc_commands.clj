0014ad63616ab1338eeb8b76f404f4934e99dfbe
Mohammad El-Abid <mohammad@el-abid.com>
2016-11-09 07:48:03 -0800

    Update parse-user to meet standards.

--- src/rocks/empty/clojure/irc/irc_commands.clj
+++ src/rocks/empty/clojure/irc/irc_commands.clj
@@ -9,13 +9,16 @@
   (.write writer (str (clojure.string/join " " args) "\r\n"))
   (.flush writer))
 
-; TODO: this isn't correct to standards
+; TODO: rename this or move into parse-prefix
 (defn parse-user
   "Returns the nickname, realname, and host of a given IRC user string"
   [line]
-  (def matcher (re-matcher #"^:(.+?)!~(.+?)@(.+?)$" line))
+  (def matcher (re-matcher #"^:(.+?)(\!.+?)?(@.+?)?$" line))
   (def results (re-find matcher))
-  {:nickname (nth results 1) :realname (nth results 2) :host (nth results 3)})
+  ; TODO: drop the ! and @ in results 2 and 3 respectively
+  (if (and (= nil (nth results 2)) (= nil (nth results 3)))
+    {:server (nth results 1)}
+    {:nickname (nth results 1) :realname (nth results 2) :host (nth results 3)}))
 
 (defn parse-params [params]
   "Parses the parameter part of an IRC message"
