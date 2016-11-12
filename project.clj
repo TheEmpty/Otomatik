2e4930178ae8e812a778baac4ea6ec539b958228
Mohammad El-Abid <mohammad@el-abid.com>
2016-11-11 21:37:31 -0800

    Add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject irc-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple IRC Bot Framework written in Clojure."
   :url "https://github.com/TheEmpty/Clojure-IRC-Bot-Core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
