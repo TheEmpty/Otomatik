b1821dda46fc60d57b2c09a87e581cbd48a659d7
Mohammad El-Abid <mohammad.elabid@gmail.com>
2016-12-11 22:16:36 -0800

    Move over to MIT license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject otomatik "0.2.0-SNAPSHOT"
   :description "A simple IRC Bot Framework written in Clojure."
   :url "https://github.com/TheEmpty/Otomatik"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
     [org.clojure/core.async "0.2.395"]
