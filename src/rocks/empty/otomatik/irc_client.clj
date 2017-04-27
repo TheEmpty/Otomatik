fe233513bd27b354749d782272a517237d4b3b42
Mohammad El-Abid <mohammad@el-abid.com>
2017-04-27 08:41:17 -0700

    Refactor plugin logic out of irc-client

--- src/rocks/empty/otomatik/irc_client.clj
+++ src/rocks/empty/otomatik/irc_client.clj
@@ -2,14 +2,12 @@
     (:require [rocks.empty.otomatik.connection-builder :as connection-builder])
     (:require [rocks.empty.otomatik.irc-commands :as irc-commands])
     (:require [rocks.empty.otomatik.irc-handlers :as irc-handlers])
-    (:require [rocks.empty.otomatik.channel-wrapper :as channel-wrapper])
+    (:require [rocks.empty.otomatik.plugin-helper :as plugin-helper])
     (:require [clojure.tools.logging :as log])
     (:require [clojure.core.async
               :as a
-              :refer [>! <! >!! <!! go go-loop chan buffer close! thread alts! alts!! timeout]])
+              :refer [>! <!! go go-loop chan]])
   )
-
-; TODO: also pull out plugins
 
 (defn write-plugin-result
   [output-channel result]
@@ -18,35 +16,13 @@
       (go (>! output-channel (str result)))
       (doseq [line result] (go (>! output-channel (str line)))))))
 
-(defn init-plugins
-  "Starts plugins that define a starting method."
-  [connection plugins]
-  (doseq [plugin plugins]
-    (if (>= 0.2 (get plugin :otomatik_version 0))
-      (when (:on-connect plugin)
-        (log/debug "Calling" (get plugin :name "a plugin's") ":on-connect by" (get plugin :author "an uknown author."))
-        ((:on-connect plugin) (channel-wrapper/create (:out connection))))
-      (when (:init plugin)
-        (log/debug "Calling" (get plugin :name "a plugin's") ":init by" (get plugin :author "an uknown author."))
-        (if (>= 0.2 (get plugin :otomatik_version 0))
-          ((:on-connect plugin) (channel-wrapper/create (:out connection)))
-          (go (write-plugin-result (:out connection)
-            ((:init plugin) {:nickname (:nickname connection)}))))))))
-
 (defn main-loop-consumer
   [connection plugins]
   (loop []
     (when-let [packet (<!! (:in connection))]
       (log/trace "Recieved from :in channel:" packet)
-      (irc-handlers/handle packet connection)
-      (doseq [plugin plugins]
-        (if (>= 0.2 (get plugin :otomatik_version 0))
-          (if (:on-message-recieved plugin)
-            ((:on-message-recieved plugin) (channel-wrapper/create (:out connection)) packet) 
-          (if (:function plugin)
-            (go (write-plugin-result
-              (:out connection)
-              ((:function plugin) packet)))))))
+      (irc-handlers/handle packet connection) ; Core handlers
+      (plugin-helper/message-recieved connection plugins packet)
       (recur))))
 
 (defn start-in-provider
@@ -81,10 +57,9 @@
   (let [input-channel (chan) output-channel (chan)]
     (let [connection (connection-builder/create options input-channel output-channel)]
       (init-connection connection options)
-      (init-plugins connection (:plugins options))
+      (plugin-helper/init-plugins connection (:plugins options))
       (log/trace "Starting up threads.")
       (start-in-provider connection)
       (start-out-consumer connection)
       (log/trace "Threads started. Starting main loop.")
       (main-loop-consumer connection (:plugins options)))))
-
---
+++ src/rocks/empty/otomatik/plugin_helper.clj
@@ -0,0 +1,40 @@
+(ns rocks.empty.otomatik.plugin-helper
+    (:require [clojure.tools.logging :as log])
+    (:require [rocks.empty.otomatik.channel-wrapper :as channel-wrapper])
+    (:require [clojure.core.async
+              :as a
+              :refer [>! go]]))
+
+(defn write-plugin-result
+  "Used for non-versioned plugins"
+  [output-channel result]
+  (when-not (= nil result)
+    (if (string? result)
+      (go (>! output-channel (str result)))
+      (doseq [line result] (go (>! output-channel (str line)))))))
+
+(defn init-plugins
+  "Starts plugins that define a starting method."
+  [connection plugins]
+  (doseq [plugin plugins]
+    (if (>= 0.2 (get plugin :otomatik_version 0))
+      (when (:on-connect plugin)
+        (log/debug "Calling" (get plugin :name "a plugin's") ":on-connect by" (get plugin :author "an uknown author."))
+        ((:on-connect plugin) (channel-wrapper/create (:out connection))))
+      (when (:init plugin)
+        (log/debug "Calling" (get plugin :name "a plugin's") ":init by" (get plugin :author "an uknown author."))
+        (if (>= 0.2 (get plugin :otomatik_version 0))
+          ((:on-connect plugin) (channel-wrapper/create (:out connection)))
+          (go (write-plugin-result (:out connection)
+            ((:init plugin) {:nickname (:nickname connection)}))))))))
+
+(defn message-recieved
+  [connection plugins packet]
+  (doseq [plugin plugins]
+    (if (>= 0.2 (get plugin :otomatik_version 0))
+      (if (:on-message-recieved plugin)
+        ((:on-message-recieved plugin) (channel-wrapper/create (:out connection)) packet) 
+      (if (:function plugin)
+        (go (write-plugin-result
+          (:out connection)
+          ((:function plugin) packet))))))))
