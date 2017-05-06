bafffd598cf25cc81a3361221790c0690d312c60
Mohammad El-Abid <mohammad@el-abid.com>
2017-05-05 19:14:41 -0700

    Adds tests for Plugin Helper
    Note: the test file is formatted poorly. Need to research more on that later.

--- src/rocks/empty/otomatik/plugin_helper.clj
+++ src/rocks/empty/otomatik/plugin_helper.clj
@@ -27,27 +27,50 @@
   [packet]
   (conj (:message packet) {:raw (:raw packet)}))
 
+(defn init-plugin-zero-dot-two
+  [connection state plugin]
+  (when (:on-connect plugin)
+    (log/debug "Calling" (get plugin :name "a plugin's") ":on-connect by" (get plugin :author "an uknown author."))
+    (idgaf
+      ((:on-connect plugin)
+        (channel-wrapper/create (:out connection))
+        (build-state-map connection state)))))
+
+(defn init-plugin-original
+  [connection state plugin]
+  (when (:init plugin)
+    (log/debug "Calling" (get plugin :name "a plugin's") ":init by" (get plugin :author "an uknown author."))
+    (idgaf (write-plugin-result (:out connection)
+      ((:init plugin) (build-state-map connection state))))))
+
 (defn init-plugins
   "Starts plugins that define a starting method."
   [connection plugins state]
   (doseq [plugin plugins]
     (if (= 0.2 (get plugin :otomatik-version 0))
-      (when (:on-connect plugin)
-        (log/debug "Calling" (get plugin :name "a plugin's") ":on-connect by" (get plugin :author "an uknown author."))
-        (idgaf
-          ((:on-connect plugin) (channel-wrapper/create (:out connection)) (build-state-map connection state))))
-      (when (:init plugin)
-        (log/debug "Calling" (get plugin :name "a plugin's") ":init by" (get plugin :author "an uknown author."))
-        (idgaf (write-plugin-result (:out connection)
-            ((:init plugin) (build-state-map connection))))))))
+      (init-plugin-zero-dot-two connection state plugin)
+      (init-plugin-original connection state plugin))))
+
+(defn message-recieved-zero-dot-two
+  [connection plugin state packet]
+  (if (:on-message-recieved plugin)
+    (let [
+      write-fn (channel-wrapper/create (:out connection))
+      message (build-message packet)
+      state-map (build-state-map connection state)
+      ]
+        (idgaf ((:on-message-recieved plugin) write-fn message state-map)))))
+
+(defn message-recieved-original
+  [connection plugin packet]
+  (if (:function plugin)
+    (idgaf (write-plugin-result
+      (:out connection)
+      ((:function plugin) packet)))))
 
 (defn message-recieved
   [connection plugins state packet]
   (doseq [plugin plugins]
     (if (= 0.2 (get plugin :otomatik-version 0))
-      (if (:on-message-recieved plugin)
-        (idgaf ((:on-message-recieved plugin) (channel-wrapper/create (:out connection)) (build-message packet) (build-state-map connection state))))
-      (if (:function plugin)
-        (idgaf (write-plugin-result
-          (:out connection)
-          ((:function plugin) packet)))))))
+      (message-recieved-zero-dot-two connection plugin state packet)
+      (message-recieved-original connection plugin packet))))
--- test/rocks/empty/otomatik/test_plugin_helper.clj
+++ test/rocks/empty/otomatik/test_plugin_helper.clj
@@ -5,6 +5,8 @@
     :as a
     :refer [>! <! >!! <!! go go-loop chan buffer close! thread alts! alts!! timeout]]))
 
+; TODO: test with a plugin that fucks shit up
+
 (deftest test-write-plugin-result-with-nil
   (let [output (chan)]
     (plugin-helper/write-plugin-result output nil)
@@ -28,3 +30,111 @@
     (let [result1 (<!! output) result2 (<!! output) results #{result1 result2}]
         (is (contains? results message1))
         (is (contains? results message2)))))
+
+(deftest test-build-state-map-adds-nickname
+  (let [connection {:nickname "Tester"} state {:something "The Other"}]
+    (let [result (plugin-helper/build-state-map connection state)]
+        (is (= (get result :nickname) "Tester"))
+        (is (= (get result :something) "The Other")))))
+
+(deftest calls-plugins-on-connect
+    (let [
+            output (chan)
+            connection {:out output}
+            state {}
+            plugin {:on-connect (fn [write state] (write "MyUniqueName"))}
+         ]
+        (plugin-helper/init-plugin-zero-dot-two connection state plugin)
+        (<!! (timeout 3))
+        (close! output)
+        (is (= "MyUniqueName" (<!! output)))))
+
+(deftest doesnt-break-if-on-connect-breaks
+    (let [
+            output (chan)
+            connection {:out output}
+            state {}
+            plugin {:on-connect (fn [write state] (write (/ 5 0)))}
+         ]
+        (plugin-helper/init-plugin-zero-dot-two connection state plugin)
+        (<!! (timeout 3))
+        (close! output)))
+
+(deftest calls-plugins-init
+    (let [
+        output (chan)
+        connection {:out output}
+        state {}
+        plugin {:init (fn [state] "MyUniqueName")}
+        ]
+        (plugin-helper/init-plugin-original connection state plugin)
+        (<!! (timeout 3))
+        (close! output)
+        (is (= "MyUniqueName" (<!! output)))))
+
+(deftest doesnt-break-if-init-breaks
+    (let [
+        output (chan)
+        connection {:out output}
+        state {}
+        plugin {:init (fn [state] (/ 5 0))}
+        ]
+        (plugin-helper/init-plugin-original connection state plugin)
+        (<!! (timeout 3))
+        (close! output)))
+
+(deftest calls-on-message-recieved
+    (let [
+        output (chan)
+        connection {:out output}
+        state {:state true}
+        packet {:message {:message true} :raw "whatever"}
+        plugin {:on-message-recieved
+            (fn [writefn message state] (do
+                (println "Message = " message)
+                (is (= true (:state state)))
+                (is (= true (:message message)))
+                (is (= "whatever" (:raw message)))
+                (writefn "Not broken")))}
+        ]
+        (plugin-helper/message-recieved-zero-dot-two connection plugin state packet)
+        (<!! (timeout 3))
+        (close! output)
+        (is (= "Not broken" (<!! output)))))
+
+(deftest doesnt-break-if-on-message-recieved-breaks
+    (let [
+        output (chan)
+        connection {:out output}
+        state {:state true}
+        packet {:message {:message true} :raw "whatever"}
+        plugin {:on-message-recieved (fn [writefn message state] (writefn (/ 5 0)))}
+        ]
+        (plugin-helper/message-recieved-zero-dot-two connection plugin state packet)
+        (<!! (timeout 3))
+        (close! output)))
+
+(deftest calls-function-for-old-plugins
+    ; I'm so sorry I named it function...
+    ; It was a proof of concept.
+    (let [
+        output (chan)
+        connection {:out output}
+        packet {:packet true}
+        plugin {:function (fn [packet] (do (is (= true (:packet packet))) "Yup"))}
+        ]
+        (plugin-helper/message-recieved-original connection plugin packet)
+        (<!! (timeout 3))
+        (close! output)
+        (is (= "Yup" (<!! output)))))
+
+(deftest doesnt-break-if-function-for-old-plugins-breaks
+    (let [
+        output (chan)
+        connection {:out output}
+        packet {:packet true}
+        plugin {:function (fn [packet] (/ 5 0))}
+        ]
+        (plugin-helper/message-recieved-original connection plugin packet)
+        (<!! (timeout 3))
+        (close! output)))
