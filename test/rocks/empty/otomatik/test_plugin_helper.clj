a4ebba4bcd7ea16e25b7f34f1ae409a571ff8d87
Mohammad El-Abid <mohammad@el-abid.com>
2017-05-05 18:13:13 -0700

    Move tests for plugin helper refactor

--- test/rocks/empty/otomatik/test_plugin_helper.clj
+++ test/rocks/empty/otomatik/test_plugin_helper.clj
@@ -1,28 +1,30 @@
-(ns rocks.empty.otomatik.test-irc-client
+(ns rocks.empty.otomatik.test-plugin-helper
   (:use [clojure.test])
-  (:require [rocks.empty.otomatik.irc-client :as client])
+  (:require [rocks.empty.otomatik.plugin-helper :as plugin-helper])
   (:require [clojure.core.async
     :as a
     :refer [>! <! >!! <!! go go-loop chan buffer close! thread alts! alts!! timeout]]))
 
 (deftest test-write-plugin-result-with-nil
   (let [output (chan)]
-    (client/write-plugin-result output nil)
+    (plugin-helper/write-plugin-result output nil)
     (<!! (timeout 3))
     (close! output)
     (is (= nil (<!! output)))))
 
 (deftest test-write-plugin-result-with-str
   (let [output (chan) message "PRIVMSG TheEmpty :Testing 1 2 3."]
-    (client/write-plugin-result output message)
+    (plugin-helper/write-plugin-result output message)
     (<!! (timeout 3))
     (close! output)
     (is (= message (<!! output)))))
 
 (deftest test-write-plugin-result-with-seq
   (let [output (chan) message1 "PRIVMSG TheEmpty :Well," message2 "PRIVMSG TheEmpty :No"]
-    (client/write-plugin-result output (seq [message1 message2]))
+    (plugin-helper/write-plugin-result output [message1 message2])
     (<!! (timeout 3))
     (close! output)
-    (is (= message1 (<!! output)))
-    (is (= message2 (<!! output)))))
+    ; Order is not guranteed, so load up and then check.
+    (let [result1 (<!! output) result2 (<!! output) results #{result1 result2}]
+        (is (contains? results message1))
+        (is (contains? results message2)))))
